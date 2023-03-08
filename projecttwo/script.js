3ee5ff347761ad2a25f05028e4eeac75e4e739ca
alisaxxd <akanlaya@pdx.edu>
2023-03-08 13:33:52 -0800

    loadbar

--- projecttwo/script.js
+++ projecttwo/script.js
@@ -2,7 +2,7 @@ let timeout;
 
 function myFunction() {
     $('.record').css('opacity', '0')
-    timeout = setTimeout(goHome, 6000);
+    timeout = setTimeout(goHome, 3000);
 }
 
 function goHome() {
